a42bab7a2f0acb1e7349a7d2581ed83734de86de
Julio Foulquie <jfoulquie@gmail.com>
2016-04-19 00:59:22 -0300

    Fix and tweak the CLI command.

--- src/JDare/Acetone/Commands/CLICommander.php
+++ src/JDare/Acetone/Commands/CLICommander.php
@@ -1,27 +1,28 @@
 <?php
 
-namespace App\Console\Commands;
+namespace JDare\Acetone\Commands;
 
 use Illuminate\Console\Command;
 use Acetone;
+use Log;
 
-class Query extends Command
+class CLICommander extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'acetone:run {type} {url}';
+    protected $signature = 'acetone:run
+                            {type : Type of command to run (purge, ban, banmany or refresh)}
+                            {url : The URL or pattern to be used with the command}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Query the Twitter api';
-
-    private $dumper;
+    protected $description = 'Run a command against Varnish';
 
     /**
      * Create a new command instance.
@@ -31,8 +32,6 @@ class Query extends Command
     public function __construct()
     {
         parent::__construct();
-
-        $this->dumper = new \Illuminate\Support\Debug\Dumper;
     }
 
     /**
@@ -45,7 +44,8 @@ class Query extends Command
         $type = $this->argument('type');
         $url = $this->argument('url');
 
-        Acetone::$type($url);
+        $response = Acetone::$type($url);
+        Log::debug($response->getReasonPhrase());
     }
 
 }
